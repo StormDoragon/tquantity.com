9f6868e9fff17cc7de3667367e0375dd8f525fbb
StormDoragon <tquantity.com@gmail.com>
2025-10-11 02:12:16 -0400

    Create notion.js

---
+++ api/notion.js
@@ -0,0 +1,6 @@
+import { Client } from '@notionhq/client';
+export default async function handler(req, res) {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const response = await notion.databases.query({ database_id: 'YOUR_DATABASE_ID' });
+  res.status(200).json(response.results);
+}
